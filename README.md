83d89ef94feaff8c0e8118c9693e7f227514d96c
ej8899 <ej8899@gmail.com>
2023-09-22 17:46:03 -0400

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # newtab
 Simple app to create a custom 'new tab' in chrome browsers - custom background images and links
+
+
+
+Requires Unsplash KEY to be into .htaccess environment variable:
+
+SetEnv UNSPLASH_API_KEY "dfdsfdfdfsf"
---
+++ server/fetch.php
@@ -0,0 +1,55 @@
+<?php
+/* 
+usage:
+http://yourwebsite.com/fetch_image.php?keyword=your_keyword_here (default is nature)
+
+example:
+https://erniejohnson.ca/tools/fetch.php?keyword=wolf
+*/
+
+
+$api_key = $_SERVER['UNSPLASH_API_KEY'];
+$keyword = isset($_GET['keyword']) ? $_GET['keyword'] : 'nature'; // Default to 'nature' if no keyword is provided
+
+// Set up the API endpoint
+$endpoint = 'https://api.unsplash.com/photos/random';
+$query = http_build_query([
+    'query' => $keyword,
+    'client_id' => $api_key,
+    'orientation' => 'landscape',
+]);
+
+$url = $endpoint . '?' . $query;
+
+// Initialize cURL session
+$ch = curl_init($url);
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+
+// Execute the cURL session and get the response
+$response = curl_exec($ch);
+
+// Check for cURL errors
+if ($response === false) {
+    echo 'Curl error: ' . curl_error($ch);
+} else {
+    // Parse the JSON response
+    $data = json_decode($response, true);
+
+    // Check if the response contains an image URL
+    if (isset($data['urls']['regular'])) {
+        $image_url = $data['urls']['regular'];
+
+        // Display the image
+        echo '<img src="' . $image_url . '" alt="Unsplash Image">';
+    } else {
+        echo 'No image found for the keyword: ' . $keyword;
+        echo '<br>api_key:' . $api_key;
+    }
+}
+
+// Close the cURL session
+curl_close($ch);
+?>
