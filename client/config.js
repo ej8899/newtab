1bcc8d8529a9401268f77f9f9ef25fe60b3ce003
ej8899 <ej8899@gmail.com>
2023-09-23 14:03:52 -0400

    add: additional customization vars

--- client/config.js
+++ client/config.js
@@ -1,4 +1,7 @@
 const configData = {
+  appVersion: "1.0",
+  appDeveloper: "ErnieJohnson.ca",
+
   imageTimer: 5,  // changes every x minutes
 
   showTimeandDate: true,  // TODO - not yet implemented - date & time always on
