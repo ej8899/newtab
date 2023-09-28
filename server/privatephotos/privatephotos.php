60797d567ec13d7e980d8844f03f22888cde7bdd
ej <ej8899@gmail.com>
2023-09-28 19:13:29 -0400

    add: randomized private photo collection response (server code)

---
+++ server/privatephotos/privatephotos.php
@@ -0,0 +1,25 @@
+<?php
+/* 
+self hosted private images to use -- put this PHP in your folder with the images 
+ - adjust client code as required
+*/
+$directory = __DIR__;
+$files = scandir($directory);
+$files = array_diff($files, array('.', '..'));
+
+// Remove "privatephotos.php" from the list
+if (($key = array_search("privatephotos.php", $files)) !== false) {
+    unset($files[$key]);
+}
+
+if (count($files) > 0) {
+    $randomIndex = array_rand($files);
+    $randomFile = $files[$randomIndex];
+    $jsonResponse = json_encode($randomFile);
+} else {
+    $jsonResponse = json_encode("No files available.");
+}
+
+header('Content-Type: application/json');
+echo $jsonResponse;
+?>
