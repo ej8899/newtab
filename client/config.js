84908d91bd4c3612a75dcb7d74179ada12018227
ej <ej8899@gmail.com>
2023-10-05 17:01:16 -0400

    update: prep config for top 10 toggle

--- client/config.js
+++ client/config.js
@@ -6,7 +6,7 @@ let configData = {
   newVersion: "0.0.0", // set this to the next version available if one exists
 
   textColor: 'white', // TODO - implement & tie to css variable
-  backgroundTheme: 'meetguinness',
+ 
   backgroundStyle: 'landscape', // TODO - implement this feature
   showIconLabels: true, // TODO - implement this feature (label under the app bar icons)
   showClock: true, // TODO - implement this feature
@@ -16,15 +16,16 @@ let configData = {
   showSearch: true, // TODO - implement this feature
   showTopten: true, // TODO - implement this feature (top 10 exists, just conditionally render)
   appBarSticky: true, // TODO - implement this -- true is app bar always open, otherwise slide open when mouse is 50px(?) within left edge
-  
-  imageTimer: 15,  // changes every x minutes
+
   widgetTransparency: 0.5, // TODO - global for the transparency level of all widgets / see CSS vars for now
   showTimeandDate: true,  // TODO - not yet implemented - date & time always on
   vignetteLevel: 0, // TODO - this should adjust the opacity of our vignette mask / see CSS vars for now
-
   newTabs: false,  // true is any bookmarks and searches open in new tab, otherwise this tab // TODO - implement htis
 
 
+  // TODO items that are done:
+  backgroundTheme: 'meetguinness',
+  imageTimer: 15,  // changes every x minutes
   clockType: null, // null for 12h format, "on" for 24h format
   gitLink: '',  
   calendarLink: '',  // TODO - if blank show basic calendar, otherwise 'none' for no calendar, or URL to google etc.
