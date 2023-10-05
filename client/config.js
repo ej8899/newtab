c29172ba956e23cde858dd9191e40e6a2d979064
ej <ej8899@gmail.com>
2023-10-05 07:26:40 -0400

    update: tweak default settings

--- client/config.js
+++ client/config.js
@@ -17,7 +17,7 @@ let configData = {
   showTopten: true, // TODO - implement this feature (top 10 exists, just conditionally render)
   appBarSticky: true, // TODO - implement this -- true is app bar always open, otherwise slide open when mouse is 50px(?) within left edge
   clockType: 12, // or 12 for 12h format
-  imageTimer: 5,  // changes every x minutes
+  imageTimer: 15,  // changes every x minutes
   widgetTransparency: 0.5, // TODO - global for the transparency level of all widgets / see CSS vars for now
   showTimeandDate: true,  // TODO - not yet implemented - date & time always on
   vignetteLevel: 0, // TODO - this should adjust the opacity of our vignette mask / see CSS vars for now
